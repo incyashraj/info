ba5f2b1765f4d389b7a9cbb3b6026ca752130036
incyashraj <65327043+incyashraj@users.noreply.github.com>
2024-10-27 16:43:47 +0530

    Update education.ts

--- src/lib/data/education.ts
+++ src/lib/data/education.ts
@@ -4,10 +4,7 @@ import type { Education } from '../types';
 export const items: Array<Education> = [
 	{
 		degree: 'Bachelor degree in Production Engineering',
-		description: 'Relevant coursework: Industrial Engineering, Business Analytics (ANNOVA, Machine Learning Algorithms),
-Industry 4.0 and Internet of Things (IoT) (Lean Production System, Smart Factories), Computer Programming
-and Problem Solving (C++), Electrical and Electronics Engineering, CAD CAM CIM, Mechatronics and
-Automation, Probability and Statistics (t-tests, chi-square tests).',
+		description: ,
 		location: 'Mumbai, India',
 		logo: Assets.Unknown,
 		name: 'Yashraj Pardeshi',
@@ -15,7 +12,7 @@ Automation, Probability and Statistics (t-tests, chi-square tests).',
 		period: { from: new Date(2021, 12), to: new Date(2025, 06) },
 		shortDescription: '',
 		slug: 'dummy-education-item',
-		subjects: ['C', 'Algorithm', 'Algebra', 'Python', 'C++', 'Java', 'English']
+		subjects: ['Industrial Engineering', 'Business Analytics (ANNOVA, Machine Learning Algorithms)', 'Industry 4.0 and Internet of Things (IoT)', '(Lean Production System, Smart Factories)', 'Computer Programming and Problem Solving (C++)', 'Electrical and Electronics Engineering', 'CAD CAM CIM', 'Mechatronics and Automation', 'Probability and Statistics', '(t-tests, chi-square tests)']
 	}
 ];
 
